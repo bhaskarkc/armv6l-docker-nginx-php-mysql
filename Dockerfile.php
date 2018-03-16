935696e5063864290109d6f4335e77a7c303cc3d
Bhaskar <bhaskar.kc@domain.com.au>
2018-03-16 12:05:15 +1100

    Maintained project tree structure.

--- Dockerfile.php
+++ Dockerfile.php
@@ -8,15 +8,14 @@ RUN apt-get update && apt-get install -y \
 	&& docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
 	&& docker-php-ext-install -j$(nproc) gd
 
-COPY docker/php/php.ini /usr/local/etc/php/conf.d/php.ini
+COPY dockeretc/php/php.ini /usr/local/etc/php/conf.d/php.ini
 COPY web /var/www/html/web
-COPY config /var/www/html/config
-COPY scripts /var/www/html/scripts
 
 ENV TZ Australia/Sydney
 
 # Create empty uploads folder owned by www-data
-RUN mkdir -p /var/www/html/web/app/uploads && chown www-data /var/www/html/web/app/uploads
+# This line is optional, but if you are using WordPress then including it would be 
+# RUN mkdir -p /var/www/html/web/wp-content/uploads && chown www-data /var/www/html/web/wp-content/uploads
 
-# Run maint script before firing up PHP-FPM
-RUN sed -i '/^set -e$/a \\n[ -z $SKIP_MAINT ] && cd /var/www/html/scripts/maintenance && php maintenance.php' /usr/local/bin/docker-php-entrypoint
+# Run PHP-FPM
+RUN /usr/local/bin/docker-php-entrypoint
